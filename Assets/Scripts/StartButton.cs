57556a50dce5158a8cbaf4c485767b7b8e56792a
Jeongmin Jeon <nicky707@hanmail.net>
2025-10-23 20:19:41 +0900

    add: button event

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections.Generic;
-using System.Collections;
 using TMPro;
+
 public class StartButton : MonoBehaviour
 {
     [SerializeField] private Button _button;
     [SerializeField] private TextMeshPro _text;
 
-
-    public void OnButtonClick()
+    public void OnPointerEnter()
     {
-        _text.text = "Stop";
+        _text.text = "Enter";
+    }
+
+    public void OnPointerExit()
+    {
+        _text.text = "Exit";
+    }
+
+    public void OnPointerDown()
+    {
+        _text.text = "Down";
+    }
+    public void OnPointerUp()
+    {
+        _text.text = "Up";
+    }
+
+    public void OnPointerClick()
+    {
+        _text.text = "Click";
     }
 }
